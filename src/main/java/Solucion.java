c897997eb04a051891b23719a254d2c6d4af8353
Alex <>
2018-04-20 09:27:25 -0500

    Solucion

--- src/main/java/Solucion.java
+++ src/main/java/Solucion.java
@@ -1,18 +1,28 @@
 public class Solucion {
     public static boolean solucion(String initialString){
         String aux = "";
-        for(int i=0;i<initialString.length()/2;i++){
+        for(int i=0;i<(initialString.length()/2)+1;i++){
             String subString = initialString.substring(0,i);
             aux = initialString.replaceAll(subString,"");
             if(aux.equals("")){
-                System.out.println(subString);
                 return true;
             }
         }
         return false;
     }
     public static void main(String[] args) {
-        System.out.println(solucion("gustavogustavogustavo"));
-        System.out.println(solucion("1231231231231234"));
+        System.out.println("Ejemplo 1");
+        System.out.println("Input: abab");
+        System.out.println("output: "+solucion("abab"));
+        System.out.println("===========================");
+        System.out.println("Ejemplo 2");
+        System.out.println("Input: aba");
+        System.out.println("output: "+solucion("aba"));
+        System.out.println("===========================");
+        System.out.println("Ejemplo 3");
+        System.out.println("Input: gustavogustavogustavo");
+        System.out.println("output: "+solucion("gustavogustavogustavo"));
+        System.out.println("===========================");
+
     }
 }
---
+++ src/test/java/SolucionTest.java
@@ -0,0 +1,41 @@
+import static org.junit.Assert.*;
+
+public class SolucionTest {
+
+    @org.junit.Test
+    public void solucion() {
+        for(int i=2;i<10000;i++){
+            assertEquals(Solucion.solucion(generadoraDeRepetitivos(i,"testing")),true);
+            assertEquals(Solucion.solucion(semiRepetitivos(i,"testing")),false);
+            assertEquals(Solucion.solucion(noRepetitivos(i)),false);
+
+        }
+    }
+
+    public String generadoraDeRepetitivos(int repeticiones, String pattern){
+        StringBuilder sb = new StringBuilder();
+        for(int i=0;i<repeticiones; i++){
+            sb.append(pattern);
+        }
+        return sb.toString();
+    }
+    //tiene un char que hace imposible que se pueda generar
+    public String semiRepetitivos(int longitud, String pattern) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < longitud; i++) {
+            sb.append(pattern);
+            if (i == longitud / 2) {
+                sb.append("-");
+            }
+        }
+        return sb.toString();
+    }
+    public String noRepetitivos(int longitud){
+        StringBuilder sb = new StringBuilder();
+        for(int i=0;i<longitud; i++){
+            sb.append(i);
+        }
+        return sb.toString();
+    }
+
+}
